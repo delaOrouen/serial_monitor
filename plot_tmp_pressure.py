bb488af920c6c6b717acd1062e57411532842cac
Rouen de la O <rouendelao@gmail.com>
2025-09-04 15:28:17 +0900

    plot_tmp_pressure.py

--- plot_tmp_pressure.py
+++ plot_tmp_pressure.py
@@ -3,6 +3,11 @@ import csv
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox
 
+Y1_MIN = 18 # Degrees C
+Y1_MAX = 42 # Degrees C
+Y2_MIN = -0.5 # Degrees C
+Y2_MAX = 8 # Degrees C
+
 # --- Handle command-line argument ---
 if len(sys.argv) < 2:
     print("Error: No CSV file provided.\nUsage: python plot_data.py <file.csv>")
@@ -46,6 +51,12 @@ data['T'] = [t / 1000.0 for t in data['T']]
 
 # --- Plotting ---
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
+fig.subplots_adjust(
+    top=0.92,
+    bottom=0.42,  # enough space for text boxes
+    hspace=0.4    # spacing between subplot rows
+)
+
 
 # Plot T1R and T2R
 line1, = ax1.plot(data['T'], data['T1R'], label='T1R', color='red')
@@ -57,34 +68,31 @@ ax1.legend()
 ax1.grid(True)
 
 # Plot P1 and P2
-line3, = ax2.plot(data['T'], data['P1'], label='P1', color='blue')
-line4, = ax2.plot(data['T'], data['P2'], label='P2', color='green')
-ax2.set_title('P1 & P2')
+line3, = ax2.plot(data['T'], data['P1'], label='Tank P1', color='blue')
+line4, = ax2.plot(data['T'], data['P2'], label='Nozzle P2', color='green')
+ax2.set_title('Tank and Nozzle Pressure')
 ax2.set_xlabel('Time (s)')
 ax2.set_ylabel('Pressure MPa')
 ax2.legend()
 ax2.grid(True)
 
-# --- Adjust layout for text boxes ---
-plt.subplots_adjust(bottom=0.35)
-
 # --- TextBox Widgets for Axis Limits ---
 
 # Axes for TextBoxes
 ax_text_xmin = plt.axes([0.15, 0.26, 0.15, 0.04])
-ax_text_xmax = plt.axes([0.35, 0.26, 0.15, 0.04])
-ax_text_y1min= plt.axes([0.15, 0.18, 0.15, 0.04])
-ax_text_y1max = plt.axes([0.35, 0.18, 0.15, 0.04])
-ax_text_y2min= plt.axes([0.15, 0.10, 0.15, 0.04])
-ax_text_y2max = plt.axes([0.35, 0.10, 0.15, 0.04])
+ax_text_xmax = plt.axes([0.42, 0.26, 0.15, 0.04])
+ax_text_y1min = plt.axes([0.15, 0.18, 0.15, 0.04])
+ax_text_y1max = plt.axes([0.42, 0.18, 0.15, 0.04])
+ax_text_y2min = plt.axes([0.15, 0.10, 0.15, 0.04])
+ax_text_y2max = plt.axes([0.42, 0.10, 0.15, 0.04])
 
 # Create TextBoxes
 text_xmin = TextBox(ax_text_xmin, 'X Min (s)', initial=str(min(data['T'])))
 text_xmax = TextBox(ax_text_xmax, 'X Max (s)', initial=str(max(data['T'])))
-text_y1min = TextBox(ax_text_y1max, 'Temp Y Min', initial=str(min(data['T1R'])))
-text_y1max = TextBox(ax_text_y1max, 'Temp Y Max', initial=str(max(data['T1R'])))
-text_y2min = TextBox(ax_text_y2max, 'Pressure Y Min', initial=str(min(data['P2'])))
-text_y2max = TextBox(ax_text_y2max, 'Pressure Y Max', initial=str(max(data['P1'])))
+text_y1min = TextBox(ax_text_y1min, 'Tmp Y Min', initial=str(Y1_MIN))
+text_y1max = TextBox(ax_text_y1max, 'Tmp Y Max', initial=str(Y1_MAX))
+text_y2min = TextBox(ax_text_y2min, 'Prs Y Min', initial=str(Y2_MIN))
+text_y2max = TextBox(ax_text_y2max, 'Prs Y Max', initial=str(Y2_MAX))
 
 # --- Update function for text input ---
 def update_axes(_):
@@ -102,13 +110,28 @@ def update_axes(_):
         ax2.set_xlim(xmin, xmax)
 
         # Set y-axis limits (min remains fixed to data min)
-        ax1.set_ylim(min(data['T1R']), y1max)
-        ax2.set_ylim(min(data['P1']), y2max)
+        ax1.set_ylim(y1min, y1max)
+        ax2.set_ylim(y2min, y2max)
 
         fig.canvas.draw_idle()
 
     except ValueError:
         print("Invalid input. Please enter valid numeric values.")
+
+xmin = float(text_xmin.text)
+xmax = float(text_xmax.text)
+y1min= float(text_y1min.text)
+y1max = float(text_y1max.text)
+y2min= float(text_y2min.text)
+y2max = float(text_y2max.text)
+
+# Set x-axis limits for both plots
+ax1.set_xlim(xmin, xmax)
+ax2.set_xlim(xmin, xmax)
+
+# Set y-axis limits (min remains fixed to data min)
+ax1.set_ylim(y1min, y1max)
+ax2.set_ylim(y2min, y2max)
 
 # --- Connect TextBoxes to update function ---
 text_xmin.on_submit(update_axes)
